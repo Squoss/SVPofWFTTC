afb5d37d791cec42c640c23f1543820f890f2f19
Squeng <paul@squeng.com>
2025-02-27 10:20:46 +0100

    took Bootstrap and Bootstrap Icons for a spin

---
+++ plapp/public/javascripts/main.js
@@ -0,0 +1,9 @@
+// cf. https://htmx.org/docs/#scripting or https://hypermedia.systems/client-side-scripting/#rsjs for more htmx-idiomatic approaches
+function switchBootstrapTheme() {
+  const body = document.body;
+  const currentTheme = body.getAttribute("data-bs-theme");
+  body.setAttribute(
+    "data-bs-theme",
+    currentTheme === "light" ? "dark" : "light"
+  );
+}
