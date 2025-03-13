6c720be8e1705d242e8fb16ce6d1d0e0db7ce8cf
Squeng <paul@squeng.com>
2025-03-13 15:14:18 +0100

    security as a forethought

--- plapp/public/javascripts/main.js
+++ plapp/public/javascripts/main.js
@@ -1,3 +1,5 @@
+document.getElementById('bsThemeSwitch').onchange = switchBootstrapTheme;
+
 // cf. https://htmx.org/docs/#scripting or https://hypermedia.systems/client-side-scripting/#rsjs for more htmx-idiomatic approaches
 function switchBootstrapTheme() {
   const body = document.body;
