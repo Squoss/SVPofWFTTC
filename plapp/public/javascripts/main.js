25fc7827b16ed45abfcf39c260dee3b3c37fdbe9
Squeng <paul@squeng.com>
2025-04-29 10:50:28 +0200

    aligned with https://getbootstrap.com/docs/5.3/customize/color-modes/#enable-dark-mode

--- plapp/public/javascripts/main.js
+++ plapp/public/javascripts/main.js
@@ -2,9 +2,9 @@ document.getElementById('bsThemeSwitch').onchange = switchBootstrapTheme;
 
 // cf. https://htmx.org/docs/#scripting or https://hypermedia.systems/client-side-scripting/#rsjs for more htmx-idiomatic approaches
 function switchBootstrapTheme() {
-  const body = document.body;
-  const currentTheme = body.getAttribute("data-bs-theme");
-  body.setAttribute(
+  const html = document.documentElement;
+  const currentTheme = html.getAttribute("data-bs-theme");
+  html.setAttribute(
     "data-bs-theme",
     currentTheme === "light" ? "dark" : "light"
   );
